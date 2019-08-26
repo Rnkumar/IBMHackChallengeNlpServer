db7f9a026e53975bb74c6a054b89312fac2b07d4
Nireshkumar R <nireshkmr.raj@gmail.com>
2019-08-26 10:10:18 +0530

    changed response format

--- app.py
+++ app.py
@@ -10,12 +10,15 @@ app = Flask(__name__)
 @app.route('/extractTags')
 def extractTags():
 	query = request.args.get('query')
-	with open('tags.txt','r') as fp:
+	with open('tags.txt','rb') as fp:
 		tags = pickle.load(fp)
 		nlp = spacy.load('en')
 		entity = Entity(keywords_list=tags)
 		nlp.add_pipe(entity,last=True)
 		# txt = unicode(query,encoding="utf-8")
 		doc =  nlp(query)
-		response = {"data":doc._.entities}
+		tagstaken = []
+		for name,index,named in doc._.entities:
+			tagstaken.append(name)
+		response = {"data":tagstaken}
 		return json.dumps(response)
